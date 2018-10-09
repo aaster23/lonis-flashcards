486348ebc0b582da9b79a19527f707ad8c608707
Danail Kirilov <vaitlspaak@gmail.com>
2018-10-09 14:53:58 +0200

    Fixed == to ===

--- javascript.js
+++ javascript.js
@@ -37,7 +37,7 @@ const createNewFlashCardLocally = (questionToPush, answerToPush) => {
 const createNewFlashCardFromForm = () => {
   const question = document.getElementsByName('qInput')[0];
   const answer = document.getElementsByName('aInput')[0];
-  if (!question.value || !answer.value || question.value.replace(/\s/g, '') == '' || answer.value.replace(/\s/g, '') == '') {
+  if (!question.value || !answer.value || question.value.replace(/\s/g, '') === '' || answer.value.replace(/\s/g, '') === '') {
     alert('Fill the forms properly!');
   } else {
     createNewFlashCardLocally(question.value, answer.value);
@@ -47,7 +47,7 @@ const createNewFlashCardFromForm = () => {
 };
 
 const deleteFlashCardQuestions = () => {
-  if (flashCards.length == 1) {
+  if (flashCards.length === 1) {
     throw alert('You must have atleast one card!');
   } else {
     flashCards = flashCards.reduce((acc, element) =>{
@@ -73,15 +73,15 @@ const shuffle = (arrayToShuffle) => {
 };
 
 const changeCard = (yesOrNo) => {
-  if (flashCards.length == 1) {
+  if (flashCards.length === 1) {
     throw alert('There are no cards, you must add some!');
   }
-  if (counter == flashCards.length - 1) {
+  if (counter === flashCards.length - 1) {
     const currentFlashCard = flashCards[counter];
     do {
       flashCards = shuffle(flashCards);
     }
-    while (flashCards[0].question == currentFlashCard.question);
+    while (flashCards[0].question === currentFlashCard.question);
     counter = 0;
     console.log(flashCards);
   }
