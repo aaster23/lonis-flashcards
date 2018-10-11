164393c5be7c3fe257cb7da30b08c6710e8d2f1b
pesh0 <peter.v.koev@gmail.com>
2018-10-11 15:39:05 +0300

    Added sidebar

--- srcModulesWithJQUERY/index.js
+++ srcModulesWithJQUERY/index.js
@@ -3,7 +3,6 @@ import { questionsField, answersField, flashCards } from './loading.js';
 import { borderBasedOnACRate, changeCard, changeCurrentCard, displayNextCard, shuffle } from './changecards.js';
 import { createNewFlashCardFromForm, createNewFlashCardLocally, deleteFlashCardQuestions } from './createAndDeleteCards.js';
 
-
 createNewFlashCardLocally('Can you name two programming paradigms important for JavaScript app developers?', 'Question 1 answer');
 createNewFlashCardLocally('What is a “closure” in JavaScript? Provide an example.', 'Question 2 answer');
 createNewFlashCardLocally('What is the difference between const, let and var?', 'Question 3 answer');
